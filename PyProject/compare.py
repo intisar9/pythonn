cf7ada2c6270610a6e642ea10325e59e3041e23c
intisar9 <94202933+intisar9@users.noreply.github.com>
2021-12-25 09:38:54 +0400

    Add files via upload

---
+++ PyProject/compare.py
@@ -0,0 +1,7 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import greater number 
+print(" Program to demonstrate the use of Pyton library-")
+num=float(input("Enter a number:"))
+print(greater number.floor (num))
+print(greater number.floor(num))
